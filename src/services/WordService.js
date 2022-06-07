8b1fca5f490800614d508b9782495ae4840147a4
jaimedue <jdue930@gmail.com>
2022-06-07 16:47:19 -0500

    backend uri

--- src/services/WordService.js
+++ src/services/WordService.js
@@ -1,5 +1,5 @@
 export const getQuizWord = async() => {
-    const URI = "https://misspelled-word-game.herokuapp.com"
+    const URI = "https://mswg-backend.herokuapp.com"
     const response = await fetch(`${URI}/api/words/quiz/word`)
     return await response.json()
 }
